e2e367601af0e7f804a8a99d737f2a69940330b9
TrilbyIcstf <trilbyicstf@gmail.com>
2025-10-09 22:18:21 -0500

    Me move now
    Started on exploration scene. Added basic movement.

---
+++ Assets/Scripts/Exploration/Player/Player_Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float maxMoveSpeed = 10.0f;
+    private float moveSpeed = 0.0f;
+    private float moveTime = 0.0f;
+
+    private Animator animator;
+
+    private Rigidbody2D rb;
+
+    private Vector2 inputDirection;
+    private Vector2 velocityDirection;
+
+    private void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        this.inputDirection.x = Input.GetAxisRaw("Horizontal");
+        this.inputDirection.y = Input.GetAxisRaw("Vertical");
+        this.inputDirection.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.inputDirection.magnitude > 0)
+        {
+            this.moveTime += Time.deltaTime * 3.5f;
+            this.velocityDirection = this.inputDirection;
+            this.animator.SetBool("Walking", true);
+        } else
+        {
+            this.moveTime -= Time.deltaTime * 5.5f;
+            this.animator.SetBool("Walking", false);
+        }
+        this.moveTime = Mathf.Clamp(this.moveTime, 0, 1);
+        this.moveSpeed = Mathf.Lerp(0.0f, this.maxMoveSpeed, this.moveTime);
+        this.rb.velocity = this.velocityDirection * this.moveSpeed;
+    }
+}
